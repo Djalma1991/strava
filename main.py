6c15010111311a0ffd9473a5deec0819e2779b3b
Djalma1991 <djalma.1991@gmail.com>
2023-03-13 14:49:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from controller import save_athlete
 
-# auth = Auth(athlete_id=65927882)
-# # data = auth.authorization_token(code="df98abd1c7ba27610ed8c8480e1d2e69f990a2cc")
-# data = auth.refresh_token()
-# print(data)
-# Athlete = Athlete(athlete_id=65927882)
-# djalminha = Athlete.get()
-# print(djalminha)
 
 djalminha = save_athlete()
 print(djalminha)
