786bee4091f05ba120c19ad561b65530165f4a20
Djalma1991 <djalma.1991@gmail.com>
2023-08-04 11:00:51 -0300

    Method to get all the activities in a date range done. Next step: improve that to take the date automatically from database

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from controller import save_athlete
+from controller import get_all_activities_time_range, activities, write_activity, save_athlete
 
-djalma = save_athlete(athlete_id=65927882)
-
-
-'''
 athlete_id_ = 65927882
-before_=1691020800
-after_=1690848000
-atividade_do_djalminha = get_all_activities_time_range(before=before_, after=after_)
+before_=1642896000
+after_=1577836800
+
+
+#djalma = save_athlete(athlete_id=athlete_id_)
+#atividade = activities(athlete_id=athlete_id_, activity_id=9578073578)
+
+atividade_do_djalminha = get_all_activities_time_range(athlete_id=athlete_id_,before=before_, after=after_)
 for i in atividade_do_djalminha:
     activity = activities(athlete_id=athlete_id_, activity_id=i)
-    write = write_activity(activity)
-'''
